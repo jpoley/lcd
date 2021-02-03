650f2d34b8cea7bc042e0ed349ee11ff0572b707
jpoley <jason.poley@gmail.com>
2021-02-02 20:26:32 -0500

    updates

---
+++ go/loginecr.sh
@@ -0,0 +1,3 @@
+export ECR_URI=$(aws ecr describe-repositories --repository-names jp-lcd |  jq -r '.repositories[].repositoryUri')
+echo $ECR_URI
+ aws ecr get-login-password | docker login --username AWS --password-stdin $ECR_URI
