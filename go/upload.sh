3a79aeaa1e50baf349920d8ed2d39f4dfa99bb9a
jpoley <jason.poley@gmail.com>
2021-02-02 21:02:14 -0500

    push fix

--- go/upload.sh
+++ go/upload.sh
@@ -1,2 +1,3 @@
-docker tag app:latest $ECR_URI/app:latest
-docker push $ECR_URI/app:latest
+docker tag app:latest app:0.0.1
+docker image tag app:0.0.1 $ECR_URI:0.0.1
+docker image push $ECR_URI:0.0.1
